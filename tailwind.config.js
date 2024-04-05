7e2694bb2e8412fc80eaf38cb8a9b17d7dbc5e52
adrianchino15 <adrian_w15@outlook.com>
2024-04-05 00:07:28 -0600

    tailwind 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './app/Views/**/*.php',
+    "./src/**/*.{html,js}",
+    
+  ],
   theme: {
     extend: {},
   },
